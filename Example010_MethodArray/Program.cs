72cbf1633489fc9c7a698b66f6d0c9e5e5c1182c
Emily <nastya.proskuryakova.98@mail.ru>
2022-10-28 00:28:12 +0300

    Поиск индекса числа из массива

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 11, 22, 34, 4, 53, 65, 75, 82 };
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+     if(array[index] == find)
+     {
+        Console.WriteLine(index);
+        break;
+     }
+    index++;
+}
