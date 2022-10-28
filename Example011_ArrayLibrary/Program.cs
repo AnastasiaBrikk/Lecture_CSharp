5266ffa0fbbf7d57c1646994bc834fc9521dda49
Emily <nastya.proskuryakova.98@mail.ru>
2022-10-28 13:07:53 +0300

    Создание функции и использование их с массивом

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int[] fill)
+{
+    int length = fill.Length;
+    int index = 0;
+    while(index<length)
+    {
+        fill[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] print)
+{
+    int count = print.Length;
+    int position = 0;
+    while(position<count)
+    {
+        Console.WriteLine(print[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
